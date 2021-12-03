d256c6dd48243f5cb5c15ef0e64580c4f536eb88
songyn <songyn@naver.com>
2021-12-03 19:53:37 +0900

    tmdrb

--- AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage1.java
+++ AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage1.java
@@ -1,5 +1,6 @@
 package com.example.tnsfl.BottomNavi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +15,7 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.tnsfl.CafeInfo;
 import com.example.tnsfl.MyAdapter;
 import com.example.tnsfl.R;
 
@@ -26,14 +28,14 @@ public class FragmentPage1 extends Fragment implements View.OnClickListener {
     private int num_page = 4;
     private CircleIndicator3 mIndicator;
     private ImageView widget_1;
-    LinearLayout Cafewidget ;
+
+    LinearLayout widget1 , widget2,widget3,widget4,widget5,widget6,widget7,widget8,widget9,widget10,widget11,widget12;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_page_1);
-
-
     }
 
     private void setContentView(int fragment_page_1) {
@@ -44,6 +46,35 @@ public class FragmentPage1 extends Fragment implements View.OnClickListener {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_page_1, container, false);
+
+        widget1 = (LinearLayout)view.findViewById(R.id.widget_1);
+        widget2 = (LinearLayout)view.findViewById(R.id.widget_2);
+        widget3 = (LinearLayout)view.findViewById(R.id.widget_3);
+        widget4 = (LinearLayout)view.findViewById(R.id.widget_4);
+        widget5 = (LinearLayout)view.findViewById(R.id.widget_5);
+        widget6 = (LinearLayout)view.findViewById(R.id.widget_6);
+        widget7 = (LinearLayout)view.findViewById(R.id.widget_7);
+        widget8 = (LinearLayout)view.findViewById(R.id.widget_8);
+        widget9 = (LinearLayout)view.findViewById(R.id.widget_9);
+        widget10 = (LinearLayout)view.findViewById(R.id.widget_10);
+        widget11 = (LinearLayout)view.findViewById(R.id.widget_11);
+        widget12 = (LinearLayout)view.findViewById(R.id.widget_12);
+
+
+        widget1.setOnClickListener(this::onClick);
+        widget2.setOnClickListener(this::onClick);
+        widget3.setOnClickListener(this::onClick);
+        widget4.setOnClickListener(this::onClick);
+        widget5.setOnClickListener(this::onClick);
+        widget6.setOnClickListener(this::onClick);
+        widget7.setOnClickListener(this::onClick);
+        widget8.setOnClickListener(this::onClick);
+        widget9.setOnClickListener(this::onClick);
+        widget10.setOnClickListener(this::onClick);
+        widget11.setOnClickListener(this::onClick);
+        widget12.setOnClickListener(this::onClick);
+
+
         mPager = view.findViewById(R.id.viewpager);
         pagerAdapter = new MyAdapter(getActivity(), num_page);
         mPager.setAdapter(pagerAdapter);
@@ -70,13 +101,6 @@ public class FragmentPage1 extends Fragment implements View.OnClickListener {
             }
         });
 
-        Cafewidget = view.findViewById(R.id.CafeWidget);
-        Cafewidget.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"goooodd",Toast.LENGTH_LONG).show();
-            }
-        });
 
         return view;
     }
@@ -89,7 +113,57 @@ public class FragmentPage1 extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-
+        Intent intent = new Intent(getActivity(), CafeInfo.class);
+        switch (view.getId()){
+            case R.id.widget_1:
+                intent.putExtra("nameid",1);
+                startActivity(intent);
+                break;
+            case R.id.widget_2:
+                intent.putExtra("nameid",2);
+                startActivity(intent);
+                break;
+            case R.id.widget_3:
+                intent.putExtra("nameid",3);
+                startActivity(intent);
+                break;
+            case R.id.widget_4:
+                intent.putExtra("nameid",4);
+                startActivity(intent);
+                break;
+            case R.id.widget_5:
+                intent.putExtra("nameid",5);
+                startActivity(intent);
+                break;
+            case R.id.widget_6:
+                intent.putExtra("nameid",6);
+                startActivity(intent);
+                break;
+            case R.id.widget_7:
+                intent.putExtra("nameid",7);
+                startActivity(intent);
+                break;
+            case R.id.widget_8:
+                intent.putExtra("nameid",8);
+                startActivity(intent);
+                break;
+            case R.id.widget_9:
+                intent.putExtra("nameid",9);
+                startActivity(intent);
+                break;
+            case R.id.widget_10:
+                intent.putExtra("nameid",10);
+                startActivity(intent);
+                break;
+            case R.id.widget_11:
+                intent.putExtra("nameid",11);
+                startActivity(intent);
+                break;
+            case R.id.widget_12:
+                intent.putExtra("nameid",12);
+                startActivity(intent);
+                break;
+        }
     }
 
 
---
+++ AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/CafeInfo.java
@@ -0,0 +1,72 @@
+package com.example.tnsfl;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CafeInfo extends AppCompatActivity {
+
+    TextView test;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cafe_infomation);
+
+        test = (TextView)findViewById(R.id.testview);
+
+        Intent getInt = getIntent();
+        int index = getInt.getExtras().getInt("nameid");
+        setResult(RESULT_OK, getInt);
+        switch (index){
+            case 1:
+                test.setText("1");
+                break;
+            case 2:
+                test.setText("2");
+                break;
+            case 3:
+                test.setText("3");
+                break;
+            case 4:
+                test.setText("4");
+                break;
+            case 5:
+                test.setText("5");
+                break;
+            case 6:
+                test.setText("6");
+                break;
+            case 7:
+                test.setText("7");
+                break;
+            case 8:
+                test.setText("8");
+                break;
+            case 9:
+                test.setText("9");
+                break;
+            case 10:
+                test.setText("10");
+                break;
+            case 11:
+                test.setText("11");
+                break;
+            case 12:
+                test.setText("12");
+                break;
+
+        }
+
+    }
+}
