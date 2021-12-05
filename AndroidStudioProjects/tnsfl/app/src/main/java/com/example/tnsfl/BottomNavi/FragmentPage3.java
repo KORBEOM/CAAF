c9b6f515b48da9cf898cb0e4f8bcd27f72e8d572
tmdrbK <ksg980920@gmail.com>
2021-12-05 18:20:23 +0900

    12050620

--- AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage3.java
+++ AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage3.java
@@ -1,10 +1,6 @@
 package com.example.tnsfl.BottomNavi;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +8,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,17 +15,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.tnsfl.DataSet.kakaResult;
+import com.example.tnsfl.Interface.kakaoApi;
 import com.example.tnsfl.R;
 
-import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class FragmentPage3 extends Fragment{
 
+    private final String API_KEY = "KakaoAK e3ed986bb1fc57e3a43528fecbf5600d";
 
     private static final String LOG_TAG = "Fragment3";
     private MapView mapView;
@@ -38,7 +41,7 @@ public class FragmentPage3 extends Fragment{
     AutoCompleteTextView autoCompleteTextView;
 
     static final String[] COUNTRIES =new String[]{
-            "스타벅스" , "탐앤탐스","스리랑가","스와핑","스콜스"
+            "스타벅스" , "탐앤탐스"
     };
 
 
@@ -65,8 +68,42 @@ public class FragmentPage3 extends Fragment{
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
             String toastMessage = ((TextView)view).getText().toString();
+            searchKeyword(toastMessage);
             Toast.makeText(getContext(),toastMessage , Toast.LENGTH_SHORT).show();
         }
+    };
+
+
+    void searchKeyword(String keyword){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://dapi.kakao.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        kakaoApi service = retrofit.create(kakaoApi.class);
+
+        Call<kakaResult> call = service.getSearch(API_KEY,"스");
+
+        call.enqueue(new Callback<kakaResult>() {
+            @Override
+            public void onResponse(Call<kakaResult> call, Response<kakaResult> response) {
+                if(response.isSuccessful()){
+                    kakaResult result =response.body();
+                    Log.d(LOG_TAG,"onResponse: 성공 , 결과 ," + result.getDocuments().get(1).getAddress_name() );
+                }
+                else{
+                    Log.d(LOG_TAG,"onResponse: 실패 " + response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<kakaResult> call, Throwable t) {
+                Log.d(LOG_TAG,"onFailure "+t.getMessage());
+
+            }
+        });
+
+
     };
 
 }
---
+++ AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/Interface/kakaoApi.java
@@ -0,0 +1,18 @@
+package com.example.tnsfl.Interface;
+
+import com.example.tnsfl.DataSet.kakaResult;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface kakaoApi {
+
+    @GET("v2/local/search/keyword.json")
+    Call<kakaResult> getSearch(
+            @Header("Authorization") String key,
+            @Query("query") String query
+    );
+}
