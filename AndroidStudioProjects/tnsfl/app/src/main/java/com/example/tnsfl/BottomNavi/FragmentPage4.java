a328874b033ead646341f8f6729fb74392260bf7
tmdqja <rkdtmdqja98@naver.com>
2021-12-09 10:17:22 +0900

    20170922

--- AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage4.java
+++ AndroidStudioProjects/tnsfl/app/src/main/java/com/example/tnsfl/BottomNavi/FragmentPage4.java
@@ -14,12 +14,20 @@ import androidx.fragment.app.Fragment;
 import com.example.tnsfl.CustomDialog;
 import com.example.tnsfl.R;
 
+<<<<<<< Updated upstream
 
 
 public class FragmentPage4 extends Fragment implements View.OnClickListener {
 
     TextView fr4_tx1,fr4_tx2,fr4_tx3,fr4_tx4;
+=======
+public class FragmentPage4 extends Fragment implements View.OnClickListener {
+>>>>>>> Stashed changes
 
+    TextView frg4_1;
+    TextView frg4_2;
+    TextView frg4_3;
+    TextView frg4_4;
 
 
     @Nullable
@@ -28,6 +36,7 @@ public class FragmentPage4 extends Fragment implements View.OnClickListener {
         setHasOptionsMenu(true);
         View view = inflater.inflate(R.layout.fragment_page_4, container, false);
 
+<<<<<<< Updated upstream
 
         fr4_tx1 = (TextView)view.findViewById(R.id.frg4_tx1);
         fr4_tx2 = (TextView)view.findViewById(R.id.frg4_tx2);
@@ -42,8 +51,25 @@ public class FragmentPage4 extends Fragment implements View.OnClickListener {
         fr4_tx4.setOnClickListener(this::onClick);
 
 
+=======
+        frg4_1 = (TextView)view.findViewById(R.id.frg4_1);
+        frg4_2 = (TextView)view.findViewById(R.id.frg4_2);
+        frg4_3 = (TextView)view.findViewById(R.id.frg4_3);
+        frg4_4 = (TextView)view.findViewById(R.id.frg4_4);
+
+        frg4_1.setOnClickListener(this::onClick);
+        frg4_2.setOnClickListener(this::onClick);
+        frg4_3.setOnClickListener(this::onClick);
+        frg4_4.setOnClickListener(this::onClick);
+
+
+
+>>>>>>> Stashed changes
 
         return view;
+
+
+
     }
 
 
@@ -57,6 +83,7 @@ public class FragmentPage4 extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+<<<<<<< Updated upstream
             case R.id.frg4_tx1:Toast toast = Toast.makeText(getContext(), "gerrard218@gmail.com", Toast.LENGTH_SHORT);
                 toast.show();
 
@@ -77,6 +104,29 @@ public class FragmentPage4 extends Fragment implements View.OnClickListener {
 
 
 
+=======
+            case R.id.frg4_1:
+                Toast toast = Toast.makeText(getContext(), "무슨 내용을 넣어야하지",Toast.LENGTH_SHORT);
+                toast.show();
+
+                break;
+            case R.id.frg4_2:
+                Toast toast1 = Toast.makeText(getContext(), "010-2259-0305",Toast.LENGTH_SHORT);
+                toast1.show();
+
+                break;
+            case R.id.frg4_3:
+                Toast toast2 = Toast.makeText(getContext(), "무슨 내용 넣어?",Toast.LENGTH_SHORT);
+                toast2.show();
+
+                break;
+            case R.id.frg4_4:
+                Toast toast3 = Toast.makeText(getContext(), "버전 1",Toast.LENGTH_SHORT);
+                toast3.show();
+
+                break;
+        }
+>>>>>>> Stashed changes
     }
 }
 
